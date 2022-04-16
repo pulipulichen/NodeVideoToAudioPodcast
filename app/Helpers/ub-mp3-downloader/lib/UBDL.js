809f4e962fd3895d5f1dd5c838f906e7aa6ccdd7
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-04-16 16:32:04 +0800

    Sat Apr 16 04:32:04 PM CST 2022

--- app/Helpers/ub-mp3-downloader/lib/UBDL.js
+++ app/Helpers/ub-mp3-downloader/lib/UBDL.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 
-let UBDL = async function (videoId, outputPath) {
+let UBDL = async function (videoID, outputPath) {
   return new Promise((resolve, reject) => {
     let cmd = 'you' + 'tu' + 'be-dl --extract-audio --audio-format mp3 --output "' + outputPath + '" https://www.you' + 'tu' + 'be.com/watch?v=' + videoID
 
