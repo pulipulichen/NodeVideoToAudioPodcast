99ec1bc8815a1c6063a1107f182665bea6c58a03
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-04-16 16:42:12 +0800

    Sat Apr 16 04:42:12 PM CST 2022

--- app/Helpers/ub-mp3-downloader/lib/UBDL.js
+++ app/Helpers/ub-mp3-downloader/lib/UBDL.js
@@ -2,9 +2,39 @@ const { exec } = require("child_process");
 
 
 let UBDL = async function (videoID, outputPath) {
-  console.log('[START UBDL]', videoID, outputPath)
+  try {
+    return UBDL1(videoID, outputPath)
+  }
+  catch (e) {
+    return UBDL2(videoID, outputPath)
+  }
+}
+
+let UBDL1 = async function (videoID, outputPath) {
+  console.log('[START UBDL1]', videoID, outputPath)
   return new Promise((resolve, reject) => {
     let cmd = 'you' + 'tu' + 'be-dl --extract-audio --audio-format mp3 --output "' + outputPath + '" https://www.you' + 'tu' + 'be.com/watch?v=' + videoID
+
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        //console.log(`error: ${error.message}`);
+        reject(error)
+        return;
+      }
+      if (stderr) {
+        reject(error)
+        return;
+      }
+      //console.log(`stdout: ${stdout}`);
+      resolve(true)
+    });
+  })
+}
+
+let UBDL2 = async function (videoID, outputPath) {
+  console.log('[START UBDL2]', videoID, outputPath)
+  return new Promise((resolve, reject) => {
+    let cmd = 'you' + 'tu' + 'be-dl -v --ignore-config -x --audio-format mp3 --output "' + outputPath + '" https://www.you' + 'tu' + 'be.com/watch?v=' + videoID
 
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
